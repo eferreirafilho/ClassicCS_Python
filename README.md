9bac67d73fe2c0a5b471e2c799e0cd0411de973f
eferreirafilho <edsonbffilho@gmail.com>
2021-12-03 11:28:18 -0300

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ## Classic CS algorithms with Python 
+
+# Chapter 1 - Small problems
+
+# Chapter 2 - Search Problems
--- fib1.py
+++ fib1.py
@@ -4,5 +4,6 @@ def fib1(n: int) -> int:
         return n
     return fib1(n-1)+fib1(n-2)
 
+
 if __name__ == "__main__":
     print(fib1(4))
